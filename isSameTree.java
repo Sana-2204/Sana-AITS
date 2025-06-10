b8304f04277822047c63af661f7e81460c051b8b
Sana-2204 <Shaiksana2224@gmail.com>
2025-06-10 13:06:38 +0530

    Add files via upload

---
+++ isSameTree.java
@@ -0,0 +1,30 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
+
+public class Solution { 
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        System.out.println("Are the trees identical? " + solution.isSameTree(p, q));
+    }
+}
